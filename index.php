0ee1f9957766cfddd224a5e03d566b13fba91165
DavozGamer <davo.z.gam3r@gmail.com>
2024-03-06 15:32:12 +0100

    aggiunti file

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz</title>
+    <title>Quiz-Daver</title>
 </head>
 <body>
-    
+    <div class="container">
+        <form action="selezionaquiz.php" method="post">
+            <div class="form-group">
+                <input type="text" name="username" id="username" placeholder="Inserisci l'username..." class="form-control" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" id="password" class="form-control" placeholder="Inserisci la password..." required>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
